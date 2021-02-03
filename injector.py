e3a75a144387215fcb95a6b19ce1497f2dd8906b
Kubeer <kubeek@onet.pl>
2021-02-04 00:13:14 +0100

    First commit

---
+++ injector.py
@@ -0,0 +1,76 @@
+import asyncio
+import time
+import websockets
+import requests
+import json
+import subprocess
+
+windows_endpoint = 'http://localhost:31337/json'
+SCRIPT_PATH = 'script.js'
+
+'''https://i.imgur.com/AlP5GMe.gif'''
+
+def getPayload(expression):
+    return {
+               'id': 1337,
+               'method': 'Runtime.evaluate',
+               'params': {'expression': expression}
+           }
+
+async def findMainWindow():
+    for x in range(5):
+        try:
+            res = requests.get(windows_endpoint)
+            if res.status_code != 200:
+                return -1
+        except Exception as e:
+            print(e)
+            pass
+        windows = res.json()
+        for window in windows:
+            url = window['webSocketDebuggerUrl']
+            try:
+                async with websockets.connect(url) as ws:
+                    await ws.send(json.dumps(getPayload('document.querySelectorAll(".item-wrap").length')))
+                    r = await ws.recv()
+                    if (json.loads(r) and json.loads(r)['result'] and json.loads(r)['result']['result'] and json.loads(r)['result']['result']['value'] and json.loads(r)['result']['result']['value'] > 0):
+                        return url
+            except Exception as e:
+                print(e)
+                pass
+        print("Did not find main window retrying... "+str(x+1)+"/5")
+
+async def injectScript(windowUrl, scriptPath):
+    try:
+        with open(scriptPath, 'r') as f:
+            payload = getPayload(f.read())
+    except Exception as e:
+        raise Exception('Error occurred while trying to open a file.')
+    print('File read successfully.')
+    try:
+        async with websockets.connect(windowUrl) as ws:
+            await ws.send(json.dumps(payload))
+            res = await ws.recv()
+            res = json.loads(res).get('result', {})
+            if not res or res.get('exceptionDetails', None):
+                error = res.get('exceptionDetails', {}).get('exception', {})
+                print('Failed injecting script {} due to {}.'.format(scriptPath, error))
+                return 0
+            print('Injection successfull.')
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    try:
+        subprocess.Popen(['Teams_o.exe', '--remote-debugging-port=31337'])
+    except Exception as e:
+        raise Exception(e)
+    time.sleep(5)
+    loop = asyncio.get_event_loop()
+    print('Trying to find main window...')
+    mainWindowUrl = loop.run_until_complete(findMainWindow())
+    if(mainWindowUrl == -1):
+        raise Exception('Error occurred while trying to find main window of Teams.exe application, please check if you set the correct port.')
+    print('Trying to inject script...')
+    loop.run_until_complete(injectScript(mainWindowUrl, SCRIPT_PATH))
---
+++ script.js
@@ -0,0 +1,7 @@
+var MAIN_BACKGROUND = "https://media1.tenor.com/images/79a1d6b4607203809a36b8ca27d3c6ef/tenor.gif"
+
+document.styleSheets[0].insertRule(".item-wrap {background: transparent !important;}");
+document.styleSheets[0].insertRule('.message-list-common::before {content:"";top:0;left:0;width:100%;height:100%;position:absolute;background:url("'+MAIN_BACKGROUND+'");background-size:cover;filter:brightness(0.5);}');
+document.styleSheets[0].insertRule(".vr-item-placeholders {display: none !important;}");
+document.styleSheets[0].insertRule(".css-43 {background: transparent !important;}");
+document.styleSheets[0].insertRule('.ms-ScrollablePane::before {content:"";top:0;left:0;width:100%;height:100%;position:absolute;background:url("'+MAIN_BACKGROUND+'");background-size:cover;filter:brightness(0.5);}');
